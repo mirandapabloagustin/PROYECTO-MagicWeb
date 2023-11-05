0634df9c658f3c0b1a6bbc6e21de4083f4d28a91
Agustin Miranda <97363454+PapasConChedar@users.noreply.github.com>
2023-11-05 09:24:13 -0300

    fix : login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { LoginComponent } from './auth/components/login/login.component';
 import { PaginaPrincipalComponent } from './modules/home/pagina-principal/pagina-principal.component';
 
 
