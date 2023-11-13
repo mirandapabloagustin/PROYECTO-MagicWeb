37d965fbebf6545bcc7a3361415dd010f78344b8
Agustin Miranda <97363454+PapasConChedar@users.noreply.github.com>
2023-11-13 01:42:44 -0300

    feature : auth /api  resposne pages

--- src/app/core/service/serviceApiScryfall/api-scryfall.service.ts
+++ src/app/core/service/serviceApiScryfall/api-scryfall.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, switchMap, of } from 'rxjs';
-import Fuse from 'fuse.js';
+import { Observable} from 'rxjs';
 import { Params } from '@angular/router';
 
 
@@ -16,30 +15,14 @@ export class ApiScryfallService {
     private http: HttpClient
   ) { }
 
-  
-  //@param: recibe un string con la consulta a la api
-  // @return: retorna un arreglo con la data de la api HttpHeaderResponse
-  public getHeaderApiResponse(queryApi:string): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/${queryApi}`, { observe: 'response' });
-  }
-
   // Api responde con una matriz de 75 cartas aleatorias
   // Trae un objeto llamado identifier  que contiene 
   // un id único para cada carta
-  public getCardsApiResponse(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/cards?&pageSize=28`);
+  public getCardsApiResponse(pageCards:number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/cards?page=${pageCards}`);
   }
 
-  public getPagePreviousApiResponse(page: number): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/cards?page=${page}&pageSize=28`)
-  }
-
-  public getPageNextApiResponse(page: number): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/cards?page=${page}&pageSize=20`)
-  }
-
-
-  public getCardsNameApiResponse(queryParams: Params): Observable<any> {
+  public getSearchCardsApiResponse(queryParams: Params): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}/cards`,{params: queryParams})
   }
 
