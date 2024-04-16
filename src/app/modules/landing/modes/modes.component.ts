dd35df02e95e7c1b413853ae10fdc584b602f148
Agustin Miranda <mirandapabloagustin@gmail.com>
2024-04-16 12:34:39 -0300

    feature mode component

--- src/app/modules/landing/modes/modes.component.ts
+++ src/app/modules/landing/modes/modes.component.ts
@@ -8,13 +8,18 @@ import { Component } from '@angular/core';
 export class ModesComponent {
   modes = [
     {
-      title: 'MTG Arena',
-      description: 'Elabora tu estrategia. Pasa a la acción. MTG Arena lleva el legendario juego estratégico de cartas a tu PC, Mac, Android e iOS.',
-      image: 'src/assets/landing/modes/articles-img/mtg-arena.jpg',
-      titleLink: 'Descargar',
+      image: '../../../../assets/landing/modes/articles-img/mtg-arena.jpg',
+      description: 'MTG Arena es un juego digital de cartas coleccionables disponible para dispositivos móviles y PC tanto para principiantes como para veteranos de Magic. Colecciona, crea y domina mazos únicos que se convertirán en leyenda. Gana recompensas y enfréntate a amigos o a otros jugadores. Juega gratis y siente la magia del juego de cartas coleccionables de fantasía original.',
+      titleLink: 'Probar Ahora',
       link: 'https://magic.wizards.com/es/mtgarena'
-    }
-      
+    },
+    {
+      image: '../../../../assets/landing/modes/articles-img/mtg-online.jpg',
+      description: 'MTG Arena es un juego digital de cartas coleccionables disponible para dispositivos móviles y PC tanto para principiantes como para veteranos de Magic. Colecciona, crea y domina mazos únicos que se convertirán en leyenda. Gana recompensas y enfréntate a amigos o a otros jugadores. Juega gratis y siente la magia del juego de cartas coleccionables de fantasía original.',
+      titleLink: 'Probar Ahora',
+      link: 'https://magic.wizards.com/es/mtgarena'
+
+    }    
   ];
 
 }
