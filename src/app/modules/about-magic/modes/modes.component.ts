e30a9268662e3048cf96ed0fb7bd00fc2a328097
Agustin Miranda <97363454+mirandapabloagustin@users.noreply.github.com>
2024-04-18 19:54:59 +0000

    fix routing files assets components

--- src/app/modules/about-magic/modes/modes.component.ts
+++ src/app/modules/about-magic/modes/modes.component.ts
@@ -10,27 +10,27 @@ export class ModesComponent {
 
   modes: CardMode[] = [
     {
-      image: '../../../../assets/landing/modes/articles-img/mtg-arena.jpg',
+      image: '../../../../assets/about-magic/modes/articles-img/mtg-arena.jpg',
       description: 'Elabora tu estrategia. Pasa a la acción. MTG Arena llega el legendario juego estratégico de cartas a tu PC, Mac, Android e iOS.',
       titleLink: 'Prueba gratis >',
       link: 'https://magic.wizards.com/es/mtgarena'
     },
     {
-      image: '../../../../assets/landing/modes/articles-img/mtg-online.jpg',
+      image: '../../../../assets/about-magic/modes/articles-img/mtg-online.jpg',
       description: 'Codeate con los mejores. MTG Online es la plataforma que lleva el juego competitivo fisico al Online. Compite!, Tradea! y Gana!.',
       titleLink: 'Desafialos Ahora >',
       link: 'https://www.mtgo.com/en/mtgo'
 
     },
     {
-      image: '../../../../assets/landing/modes/articles-img/mtg-fisico.jpg',
+      image: '../../../../assets/about-magic/modes/articles-img/mtg-fisico.jpg',
       description: 'Coleccioná, intercambia y jugá con tus cartas favoritas. Descubre en la tienda oficial los productos más recientes. Elige tu mazo y juega!.',
       titleLink: 'Explora la tienda >',
       link: 'https://magic.wizards.com/es/products'
 
     },
     {
-      image: '../../../../assets/landing/modes/articles-img/mtg-championship.jpg',
+      image: '../../../../assets/about-magic/modes/articles-img/mtg-championship.jpg',
       description: 'Sé el mejor. Conviértete en el campeón de MTG. Participa en los torneos más importantes del mundo.',
       titleLink: 'Más informacion >',
       link: 'https://magic.gg/'
