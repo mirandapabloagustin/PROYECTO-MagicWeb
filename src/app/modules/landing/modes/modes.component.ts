ab82696cd1c92da7a9702fcffbd02ca382e2ef0b
Agustin Miranda <97363454+mirandapabloagustin@users.noreply.github.com>
2024-04-18 00:52:05 +0000

    update mode component

--- src/app/modules/landing/modes/modes.component.ts
+++ src/app/modules/landing/modes/modes.component.ts
@@ -9,17 +9,32 @@ export class ModesComponent {
   modes = [
     {
       image: '../../../../assets/landing/modes/articles-img/mtg-arena.jpg',
-      description: 'MTG Arena es un juego digital de cartas coleccionables disponible para dispositivos móviles y PC tanto para principiantes como para veteranos de Magic. Colecciona, crea y domina mazos únicos que se convertirán en leyenda. Gana recompensas y enfréntate a amigos o a otros jugadores. Juega gratis y siente la magia del juego de cartas coleccionables de fantasía original.',
-      titleLink: 'Probar Ahora',
+      description: 'Elabora tu estrategia. Pasa a la acción. MTG Arena llega el legendario juego estratégico de cartas a tu PC, Mac, Android e iOS.',
+      titleLink: 'Prueba gratis >',
       link: 'https://magic.wizards.com/es/mtgarena'
     },
     {
       image: '../../../../assets/landing/modes/articles-img/mtg-online.jpg',
-      description: 'MTG Arena es un juego digital de cartas coleccionables disponible para dispositivos móviles y PC tanto para principiantes como para veteranos de Magic. Colecciona, crea y domina mazos únicos que se convertirán en leyenda. Gana recompensas y enfréntate a amigos o a otros jugadores. Juega gratis y siente la magia del juego de cartas coleccionables de fantasía original.',
-      titleLink: 'Probar Ahora',
-      link: 'https://magic.wizards.com/es/mtgarena'
+      description: 'Codeate con los mejores. MTG Online es la plataforma que lleva el juego competitivo fisico al Online. Compite!, Tradea! y Gana!.',
+      titleLink: 'Desafialos Ahora >',
+      link: 'https://www.mtgo.com/en/mtgo'
 
-    }    
+    },
+    {
+      image: '../../../../assets/landing/modes/articles-img/mtg-fisico.jpg',
+      description: 'Colecciona las cartas más raras y poderosas. Descubre el mundo de Magic: The Gathering a través de sus cartas físicas.',
+      titleLink: 'Explora la tienda >',
+      link: 'https://magic.wizards.com/es/products'
+
+    },
+    {
+      image: '../../../../assets/landing/modes/articles-img/mtg-championship.jpg',
+      description: 'Sé el mejor. Conviértete en el campeón de MTG. Participa en los torneos más importantes del mundo.',
+      titleLink: 'Más informacion >',
+      link: 'https://magic.gg/'
+
+    }
+
   ];
 
 }
