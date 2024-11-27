6be63a0d6fb93d3703e542c44e069ffa61d7ff74
Agustin Miranda <97363454+mirandapabloagustin@users.noreply.github.com>
2024-11-27 13:45:30 +0000

    Feature: actualizar información de la carta "Activated Sleeper" y cambiar título de legalidades a "Formatos Legales"

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -35,90 +35,64 @@ export class MainComponent {
   prueba=
   {
     "object": "card",
-    "id": "29eb0e40-d184-423f-8484-ee288fda00cd",
-    "oracle_id": "02718076-4c71-4bf5-988f-e6f94fbf0aef",
+    "id": "343d01cf-9806-4c2d-a993-ddc9ed248d7f",
+    "oracle_id": "e12da2a1-e390-4487-b154-69e439cb7b05",
     "multiverse_ids": [
-      555425
+      580490
     ],
-    "mtgo_id": 98667,
-    "arena_id": 80375,
-    "tcgplayer_id": 269089,
-    "cardmarket_id": 652210,
-    "name": "Spara's Adjudicators",
+    "mtgo_id": 103030,
+    "tcgplayer_id": 283686,
+    "cardmarket_id": 672630,
+    "name": "Activated Sleeper",
     "lang": "en",
-    "released_at": "2022-04-29",
-    "uri": "https://api.scryfall.com/cards/29eb0e40-d184-423f-8484-ee288fda00cd",
-    "scryfall_uri": "https://scryfall.com/card/snc/224/sparas-adjudicators?utm_source=api",
+    "released_at": "2022-09-09",
+    "uri": "https://api.scryfall.com/cards/343d01cf-9806-4c2d-a993-ddc9ed248d7f",
+    "scryfall_uri": "https://scryfall.com/card/dmc/24/activated-sleeper?utm_source=api",
     "layout": "normal",
     "highres_image": true,
     "image_status": "highres_scan",
     "image_uris": {
-      "small": "https://cards.scryfall.io/small/front/2/9/29eb0e40-d184-423f-8484-ee288fda00cd.jpg?1664413807",
-      "normal": "https://cards.scryfall.io/normal/front/2/9/29eb0e40-d184-423f-8484-ee288fda00cd.jpg?1664413807",
-      "large": "https://cards.scryfall.io/large/front/2/9/29eb0e40-d184-423f-8484-ee288fda00cd.jpg?1664413807",
-      "png": "https://cards.scryfall.io/png/front/2/9/29eb0e40-d184-423f-8484-ee288fda00cd.png?1664413807",
-      "art_crop": "https://cards.scryfall.io/art_crop/front/2/9/29eb0e40-d184-423f-8484-ee288fda00cd.jpg?1664413807",
-      "border_crop": "https://cards.scryfall.io/border_crop/front/2/9/29eb0e40-d184-423f-8484-ee288fda00cd.jpg?1664413807"
+      "small": "https://cards.scryfall.io/small/front/3/4/343d01cf-9806-4c2d-a993-ddc9ed248d7f.jpg?1673304862",
+      "normal": "https://cards.scryfall.io/normal/front/3/4/343d01cf-9806-4c2d-a993-ddc9ed248d7f.jpg?1673304862",
+      "large": "https://cards.scryfall.io/large/front/3/4/343d01cf-9806-4c2d-a993-ddc9ed248d7f.jpg?1673304862",
+      "png": "https://cards.scryfall.io/png/front/3/4/343d01cf-9806-4c2d-a993-ddc9ed248d7f.png?1673304862",
+      "art_crop": "https://cards.scryfall.io/art_crop/front/3/4/343d01cf-9806-4c2d-a993-ddc9ed248d7f.jpg?1673304862",
+      "border_crop": "https://cards.scryfall.io/border_crop/front/3/4/343d01cf-9806-4c2d-a993-ddc9ed248d7f.jpg?1673304862"
     },
-    "mana_cost": "{2}{G}{W}{U}",
-    "cmc": 5.0,
-    "type_line": "Creature — Cat Citizen",
-    "oracle_text": "When Spara's Adjudicators enters, target creature an opponent controls can't attack or block until your next turn.\n{2}, Exile Spara's Adjudicators from your hand: Target land gains \"{T}: Add {G}, {W}, or {U}\" until Spara's Adjudicators is cast from exile. You may cast Spara's Adjudicators for as long as it remains exiled.",
-    "power": "4",
-    "toughness": "4",
+    "mana_cost": "{2}{B}",
+    "cmc": 3.0,
+    "type_line": "Creature — Phyrexian Shapeshifter",
+    "oracle_text": "Flash\nYou may have Activated Sleeper enter as a copy of any creature card in a graveyard that was put there from the battlefield this turn, except it's a Phyrexian in addition to its other types.",
+    "power": "0",
+    "toughness": "0",
     "colors": [
-      "G",
-      "U",
-      "W"
+      "B"
     ],
     "color_identity": [
-      "G",
-      "U",
-      "W"
+      "B"
     ],
-    "keywords": [],
-    "produced_mana": [
-      "G",
-      "U",
-      "W"
-    ],
-    "all_parts": [
-      {
-        "object": "related_card",
-        "id": "93df6b13-d898-4afe-a60c-828933f5be32",
-        "component": "combo_piece",
-        "name": "Spara's Adjudicators",
-        "type_line": "Creature — Cat Citizen",
-        "uri": "https://api.scryfall.com/cards/93df6b13-d898-4afe-a60c-828933f5be32"
-      },
-      {
-        "object": "related_card",
-        "id": "f564085e-2abd-4ecc-9b7a-19a69ddabc84",
-        "component": "combo_piece",
-        "name": "A-Spara's Adjudicators",
-        "type_line": "Creature — Cat Citizen",
-        "uri": "https://api.scryfall.com/cards/f564085e-2abd-4ecc-9b7a-19a69ddabc84"
-      }
+    "keywords": [
+      "Flash"
     ],
     "legalities": {
       "standard": "not_legal",
       "future": "not_legal",
       "historic": "not_legal",
-      "timeless": "legal",
-      "gladiator": "legal",
-      "pioneer": "legal",
-      "explorer": "legal",
-      "modern": "legal",
+      "timeless": "not_legal",
+      "gladiator": "not_legal",
+      "pioneer": "not_legal",
+      "explorer": "not_legal",
+      "modern": "not_legal",
       "legacy": "legal",
-      "pauper": "legal",
+      "pauper": "not_legal",
       "vintage": "legal",
-      "penny": "legal",
+      "penny": "not_legal",
       "commander": "legal",
       "oathbreaker": "legal",
       "standardbrawl": "not_legal",
       "brawl": "not_legal",
       "alchemy": "not_legal",
-      "paupercommander": "legal",
+      "paupercommander": "not_legal",
       "duel": "legal",
       "oldschool": "not_legal",
       "premodern": "not_legal",
@@ -126,8 +100,7 @@ export class MainComponent {
     },
     "games": [
       "paper",
-      "mtgo",
-      "arena"
+      "mtgo"
     ],
     "reserved": false,
     "foil": true,
@@ -140,56 +113,51 @@ export class MainComponent {
     "promo": false,
     "reprint": false,
     "variation": false,
-    "set_id": "df837242-8c15-42e4-b049-c933a02dc501",
-    "set": "snc",
-    "set_name": "Streets of New Capenna",
-    "set_type": "expansion",
-    "set_uri": "https://api.scryfall.com/sets/df837242-8c15-42e4-b049-c933a02dc501",
-    "set_search_uri": "https://api.scryfall.com/cards/search?order=set&q=e%3Asnc&unique=prints",
-    "scryfall_set_uri": "https://scryfall.com/sets/snc?utm_source=api",
-    "rulings_uri": "https://api.scryfall.com/cards/29eb0e40-d184-423f-8484-ee288fda00cd/rulings",
-    "prints_search_uri": "https://api.scryfall.com/cards/search?order=released&q=oracleid%3A02718076-4c71-4bf5-988f-e6f94fbf0aef&unique=prints",
-    "collector_number": "224",
+    "set_id": "78076b27-f888-4723-b27c-44074accd261",
+    "set": "dmc",
+    "set_name": "Dominaria United Commander",
+    "set_type": "commander",
+    "set_uri": "https://api.scryfall.com/sets/78076b27-f888-4723-b27c-44074accd261",
+    "set_search_uri": "https://api.scryfall.com/cards/search?order=set&q=e%3Admc&unique=prints",
+    "scryfall_set_uri": "https://scryfall.com/sets/dmc?utm_source=api",
+    "rulings_uri": "https://api.scryfall.com/cards/343d01cf-9806-4c2d-a993-ddc9ed248d7f/rulings",
+    "prints_search_uri": "https://api.scryfall.com/cards/search?order=released&q=oracleid%3Ae12da2a1-e390-4487-b154-69e439cb7b05&unique=prints",
+    "collector_number": "24",
     "digital": false,
-    "rarity": "common",
-    "watermark": "brokers",
+    "rarity": "rare",
     "card_back_id": "0aeebaf5-8c7d-4636-9e82-8c27447861f7",
-    "artist": "Aaron J. Riley",
+    "artist": "Mathias Kollros",
     "artist_ids": [
-      "f21b412c-aef5-4d15-88be-4f40615acde3"
+      "2ae0dd12-047c-4efb-80d7-bccc3bac5fba"
     ],
-    "illustration_id": "e05da8aa-ffa6-4e02-8280-58fc8a72a778",
+    "illustration_id": "5dc4ac5e-383d-4747-b849-bb44ab46bfd1",
     "border_color": "black",
     "frame": "2015",
+    "security_stamp": "oval",
     "full_art": false,
     "textless": false,
-    "booster": true,
+    "booster": false,
     "story_spotlight": false,
-    "edhrec_rank": 18328,
-    "penny_rank": 12306,
-    "preview": {
-      "source": "Riley Knight",
-      "source_uri": "https://twitter.com/RLYKNGHT/status/1514758512718794752",
-      "previewed_at": "2022-04-15"
-    },
+    "edhrec_rank": 6486,
+    "penny_rank": 6339,
     "prices": {
-      "usd": "0.02",
-      "usd_foil": "0.18",
+      "usd": "0.95",
+      "usd_foil": "1.83",
       "usd_etched": null,
-      "eur": "0.04",
-      "eur_foil": "0.10",
-      "tix": "0.01"
+      "eur": "0.72",
+      "eur_foil": "1.21",
+      "tix": "0.45"
     },
     "related_uris": {
-      "gatherer": "https://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=555425&printed=false",
-      "tcgplayer_infinite_articles": "https://tcgplayer.pxf.io/c/4931599/1830156/21018?subId1=api&trafcat=infinite&u=https%3A%2F%2Finfinite.tcgplayer.com%2Fsearch%3FcontentMode%3Darticle%26game%3Dmagic%26partner%3Dscryfall%26q%3DSpara%2527s%2BAdjudicators",
-      "tcgplayer_infinite_decks": "https://tcgplayer.pxf.io/c/4931599/1830156/21018?subId1=api&trafcat=infinite&u=https%3A%2F%2Finfinite.tcgplayer.com%2Fsearch%3FcontentMode%3Ddeck%26game%3Dmagic%26partner%3Dscryfall%26q%3DSpara%2527s%2BAdjudicators",
-      "edhrec": "https://edhrec.com/route/?cc=Spara%27s+Adjudicators"
+      "gatherer": "https://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=580490&printed=false",
+      "tcgplayer_infinite_articles": "https://tcgplayer.pxf.io/c/4931599/1830156/21018?subId1=api&trafcat=infinite&u=https%3A%2F%2Finfinite.tcgplayer.com%2Fsearch%3FcontentMode%3Darticle%26game%3Dmagic%26partner%3Dscryfall%26q%3DActivated%2BSleeper",
+      "tcgplayer_infinite_decks": "https://tcgplayer.pxf.io/c/4931599/1830156/21018?subId1=api&trafcat=infinite&u=https%3A%2F%2Finfinite.tcgplayer.com%2Fsearch%3FcontentMode%3Ddeck%26game%3Dmagic%26partner%3Dscryfall%26q%3DActivated%2BSleeper",
+      "edhrec": "https://edhrec.com/route/?cc=Activated+Sleeper"
     },
     "purchase_uris": {
-      "tcgplayer": "https://tcgplayer.pxf.io/c/4931599/1830156/21018?subId1=api&u=https%3A%2F%2Fwww.tcgplayer.com%2Fproduct%2F269089%3Fpage%3D1",
-      "cardmarket": "https://www.cardmarket.com/en/Magic/Products/Singles/Streets-of-New-Capenna/Sparas-Adjudicators?referrer=scryfall&utm_campaign=card_prices&utm_medium=text&utm_source=scryfall",
-      "cardhoarder": "https://www.cardhoarder.com/cards/98667?affiliate_id=scryfall&ref=card-profile&utm_campaign=affiliate&utm_medium=card&utm_source=scryfall"
+      "tcgplayer": "https://tcgplayer.pxf.io/c/4931599/1830156/21018?subId1=api&u=https%3A%2F%2Fwww.tcgplayer.com%2Fproduct%2F283686%3Fpage%3D1",
+      "cardmarket": "https://www.cardmarket.com/en/Magic/Products/Singles/Commander-Dominaria-United/Activated-Sleeper?referrer=scryfall&utm_campaign=card_prices&utm_medium=text&utm_source=scryfall",
+      "cardhoarder": "https://www.cardhoarder.com/cards/103030?affiliate_id=scryfall&ref=card-profile&utm_campaign=affiliate&utm_medium=card&utm_source=scryfall"
     }
   }
   
