52c6c74764ca0f6ab0be3ce72e5d1404621bbaf2
Agustin Miranda <mirandapabloagustin@gmail.com>
2025-01-23 08:24:50 -0300

    Fix: Problemas con el logueo de usuarios

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,5 +1,5 @@
 export const enviorment = {
     production: false,
     apiUrl: 'https://api.scryfall.com/',
-    apiUser: 'https://stunning-acorn-p57pqwj7g6r279j7-3001.app.github.dev/',
+    apiUser: 'http://localhost:3000',
 }
