f9958a859740b658329829d75fa1918b73139b86
Agustin Miranda <mirandapabloagustin@gmail.com>
2025-02-05 09:58:07 -0300

    Feature: se implemento logica de filtro en componente filter-decks, implementacion de estilos al componente.

--- src/app/decks/components/filter-decks/filter-decks.component.ts
+++ src/app/decks/components/filter-decks/filter-decks.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
-import { SearchComponent } from "@shared/search/search.component";
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-filter-decks',
   standalone: true,
-  imports: [SearchComponent],
+  imports: [ReactiveFormsModule],
   templateUrl: './filter-decks.component.html',
   styleUrl: './filter-decks.component.css'
 })
 export class FilterDecksComponent {
+  formColor: any;
+
+  constructor(
+    private _formBuilder: FormBuilder,
+  ) {
+    this.formColor = this._formBuilder.group({
+      white: false,
+      blue: false,
+      black: false,
+      red: false,
+      green: false,
+      all: true,
+      mana: 'all'
+    });
+
+  }
+
+  toggleCheckboxColor(color:string){
+    const value = this.formColor.get(color).value;
+    const status = this.formColor.get(color).setValue(!value);
+    this.changeStyleToActive(color+'-select', !value);
+    console.log(this.formColor.value);
+  }
+
+  changeStyleToActive(color:string, flag:boolean){
+    const element = document.getElementById(color);
+    flag ? element?.classList.add('active') : element?.classList.remove('active');
+  }
 
 }
